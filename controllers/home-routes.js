2649f5605ed363d82e5f92a0896abc90c50e3d41
Laertes <maclellanj15@gmail.com>
2022-03-22 15:20:00 -0500

    login/logout functions

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,20 +2,6 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
-// router.get('/', (req, res) => {
-//     res.render('homepage', {
-//         id: 1,
-//         post_url: 'https://handlebarsjs.com/guide/',
-//         title: 'Handlebars Docs',
-//         created_at: new Date(),
-//         vote_count: 10,
-//         comments: [{}, {}],
-//         user: {
-//             username: 'test_user'
-//         }
-//     });
-// });
-
 router.get('/', (req, res) => {
     Post.findAll({
         attributes: [
@@ -52,5 +38,20 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
     });
 });
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    
+    res.render('login');
+});
+
+router.get('/', (req, res) => {
+    console.log(req.session);
+
+
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,11 +6,29 @@ const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
+
+// session
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: process.env.secret,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
